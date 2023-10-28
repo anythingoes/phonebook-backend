9d81931191dec1b50067cb8fe05c976cdcad8b1f
Rohit <rohitsaraff@gmail.com>
2023-10-28 12:30:36 +0545

    Add frontend build

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
 );
 
 app.use(cors());
+app.use(express.static("dist")); //middleware used to serve the static index.html file from the dist directory which containts frontend react code
 
 let persons = [
   {
